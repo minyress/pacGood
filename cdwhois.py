b0d9c35a365f3d545f1bba64c1eb0ac859f7c9e1
root <root@vps_JP.(none)>
2015-01-29 16:48:19 +0800

    first fuck

---
+++ cdwhois.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import re
+import os
+
+gfile = '/gfwlist.txt'
+
+def cwho(domain):
+	cmd = '''dig %s ns |grep "a.gtld-servers.net. nstld.verisign-grs.com." > /dev/null'''%domain
+	dig = os.system(cmd)
+	if dig:
+		#print domain,'ok'
+		pass
+	else:
+		print domain
+
+def main():
+	f = open(gfile)
+	for i in f.readlines():
+		a = re.search('''"(.+)": 1,''',i)
+		if a:
+			d = a.group(1)
+			cwho(d)
+	f.close()
+
+
+main()
