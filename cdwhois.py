d4653e581f5c2466c4f0446ab6a8bd8b30508ced
liubingchuan <liubingchuan@daixiaomi.com>
2019-11-13 15:57:37 +0800

    update

--- cdwhois.py
+++ cdwhois.py
@@ -2,25 +2,32 @@
 import re
 import os
 
-gfile = 'gfwlist.txt'
+gfile = 'pac.txt'
 
 def cwho(domain):
-	cmd = '''dig %s ns |grep "a.gtld-servers.net. nstld.verisign-grs.com." > /dev/null'''%domain
-	dig = os.system(cmd)
-	if dig:
-		#print domain,'ok'
-		pass
-	else:
-		print domain
+    cmd = '''dig %s ns @8.8.8.8 |grep -v "^;" |grep -v "^$" |grep 'IN\tSOA' > /dev/null''' % domain
+    dig = os.system(cmd)
+    if dig:
+        #print domain,'ok'
+        pass
+    else:
+        print(domain)
 
 def main():
-	f = open(gfile)
-	for i in f.readlines():
-		a = re.search('''"(.+)": 1,''',i)
-		if a:
-			d = a.group(1)
-			cwho(d)
-	f.close()
+    f = open(gfile)
+    for i in f.readlines():
+        a = re.search('''"(.+)": 1,''',i)
+        if a:
+            d = a.group(1)
+            cwho(d)
+    f.close()
 
 main()
+
+
+
+'''
+for i in `cat del.txt`; do echo  sed -ip "/$i/d" pac.txt ;done
+'''
+
 
